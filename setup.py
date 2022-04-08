0b9355c6f7e18cc31d21fc1f4989d7b255960247
Michael Herrmann <michael@herrmann.io>
2022-04-08 12:28:28 +0200

    Update project URL in setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     description = "Automatically send error emails about failing Celery tasks in your Django project.",
     license = "MIT",
     keywords = "celery error emails django",
-    url = "https://github.com/mherrmann/celery_error_emails",
-    long_description = "For more details, please see https://github.com/mherrmann/celery_error_emails",
+    url = "https://github.com/mherrmann/celery-error-emails",
+    long_description = "For more details, please see https://github.com/mherrmann/celery-error-emails",
     py_modules=['celery_error_emails'],
     classifiers = [
         "Development Status :: 3 - Alpha",
